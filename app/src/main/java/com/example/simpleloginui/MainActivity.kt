59dd09970b33524899d519f69a07704bade3e37f
Amadeus Kaczmarek <amadeuskaczmarek@gmail.com>
2025-05-21 00:49:50 -0600

    final

--- app/src/main/java/com/example/simpleloginui/MainActivity.kt
+++ app/src/main/java/com/example/simpleloginui/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.simpleloginui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.media3.common.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var login_button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        login_button = findViewById(R.id.login_button)
+
+        login_button.setOnClickListener(){
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
         }
+
     }
 }
